4f9cea781b5890d20eacc97489ae2feb9209121a
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-09 21:31:45 +0300

    Update contentfulPosts.js

--- src/utils/contentfulPosts.js
+++ src/utils/contentfulPosts.js
@@ -1,1 +1,17 @@
+import { createClient } from "contentful";
+//const dotenv =  require('dotenv');
+//dotenv.config()
+const space = 9heip63yijn8
+const accessToken = CQ4Ja0XdmfMso-gIfbIxBnEUBCFwIttPKT-R57fvZek
+const client = require('contentful').createClient({
+  space: space,
+  accessToken: accessToken,
+})
 
+export async function fetchEntries() {
+  const entries = await client.getEntries()
+  if (entries.items) return entries.items
+  console.log(`Error getting Entries for ${contentType.name}.`)
+}
+
+export default { fetchEntries }
