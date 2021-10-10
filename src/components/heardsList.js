945d531dbf031cc42f105e75b7e4cd8eeea94b41
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-10 15:00:19 +0300

    Create heardsList.js

---
+++ src/components/heardsList.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+//import * as Markdown from 'react-markdown';
+
+
+const HeardsList = (props) =>{
+
+                
+                
+            return (
+  <div>
+    <h1>{props.title}</h1>
+   <p>{props.body}</p>
+  </div>
+)
+                
+            
+       }
+   
+
+export default HeardsList;
+
+
+
+
