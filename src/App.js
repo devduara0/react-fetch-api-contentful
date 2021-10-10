bae1ad90ffb66f1b5c10424ec8d520fd35eba99f
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-10 21:07:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
 import GlobalStyles from './components/styles/Global'
+import Title from './components/styles/Titlr'
 
 const theme = {
   colors: {
@@ -18,7 +19,9 @@ function App() {
       <>
         <GlobalStyles />
         <Header />
-       
+       <Title>
+       Test
+      </Title>
        
       </>
     </ThemeProvider>
