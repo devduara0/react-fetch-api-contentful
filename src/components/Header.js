6c1b5b431b9a83ebfe37c2c4e9d32c007c84c856
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-10 14:54:37 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,49 @@
+import React,{ Fragment, Component } from 'react';
+import { createClient } from "contentful";
+//import './content.scss';
+//import HomeIterm from './homeIterm';
+//import Banner from './Banner';
+//import Hero from './Hero';
+//import Banner from './banner'
+import HeardList from './heardsList';
 
+class Header extends Component {
+constructor() {
+    super();
+    this.state = { heards: [] };
+    this.client = createClient({
+      accessToken:
+        "CQ4Ja0XdmfMso-gIfbIxBnEUBCFwIttPKT-R57fvZek",
+      space: "9heip63yijn8"
+    });
+}
+  
+
+componentDidMount() {
+    this.fetchHeards().then(this.setHeards);
+  }
+fetchHeards = () => this.client.getEntries()
+
+setHeards = response => {
+    this.setState({
+      heards: response.items
+    })
+  }
+
+    render(){
+        return(
+            <Fragment>
+       
+    
+    
+    <div className="tr">
+      { this.state.heards.map(({fields}, i) =>
+        <HeardsList key={i} {...fields} />
+      )}
+     </div>
+    </Fragment>
+        )
+    }
+}
+
+export default Header;
