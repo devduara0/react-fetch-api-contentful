272188df262cd8e8787cd678d7f76a9b080df104
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-10 15:10:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import GlobalStyles from './components/styles/Global'
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+       
+       
+      </>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
