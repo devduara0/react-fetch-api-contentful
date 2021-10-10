87d29518bfff704b5340f0655e6744778410daa6
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-10 19:54:20 +0300

    Update Global.js

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -7,17 +7,12 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  mwili {
+  body {
     background: ${({ theme }) => theme.colors.body};
     color: hsl(192, 100%, 9%);
     font-family: 'Poppins', sans-serif;
     font-size: 1.15em;
     margin: 0;
-  }
-
-   body {
-    background: red;
-    
   }
 
 
