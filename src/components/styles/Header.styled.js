c0ce5713275ae0cf8d34c072f0245331025631ae
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-11 16:10:43 +0300

    Update Header.styled.js

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
-  background-color: #ff0000;
+  background-color: ${({ theme }) => theme.colors.header};
   padding: 40px 0;
 `
 
