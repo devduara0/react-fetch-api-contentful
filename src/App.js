af4b8a82604087960eb66a158993952279326187
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-12 21:16:05 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const App = () => {
+  return (
+    <main>
+      
+      <h1>Hello world!</h1>
+      
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default App
