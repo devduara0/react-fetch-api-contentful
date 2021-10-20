b363220bb2f23eb85c727a810616d3ce5a98b33e
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-20 10:29:16 +0300

    Update Buttonbg.js

--- src/components/styles/Buttonbg.js
+++ src/components/styles/Buttonbg.js
@@ -8,7 +8,7 @@ export const Buttonbg = styled.button`
   font-size: 16px;
   font-weight: 700;
   padding: 15px 60px;
-  background-color: ${({ bg }) => bg || '#fff'};
+  background-color: ${({ theme }) => theme.colors.button};
   color: ${({ color }) => color || '#333'};
 
   &:hover {
