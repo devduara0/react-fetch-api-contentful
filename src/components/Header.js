0b6b583ae35f066cade56e2aab56e9b2ba3e87cc
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-21 12:16:43 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ function Header() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
+          Authorization: `Bearer ${accessToken}`
         },
         body: JSON.stringify({ query }),
       })
