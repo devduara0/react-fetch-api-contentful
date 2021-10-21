efb73846ae3f9a1308e75962ab0ee5f566083acb
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-21 11:59:32 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import { Container } from './styles/Container.styled'
 import { Flex } from './styles/Flex.styled'
 //import { Button } from './styles/Button.styled'
 import { Buttonbg } from './styles/Buttonbg'
-
+const dotenv =  require('dotenv');
+dotenv.config()
+const space = process.env.REAT_PUBLIC_CONTENTFUL_SPACE_ID1
+const accessToken = process.env.REAT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
 const query = `
 {
    headerCollection{
@@ -27,11 +30,11 @@ function Header() {
 
   useEffect(() => {
     window
-      .fetch(`https://graphql.contentful.com/content/v1/spaces/9heip63yijn8/`, {
+      .fetch(space, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer CQ4Ja0XdmfMso-gIfbIxBnEUBCFwIttPKT-R57fvZek",
+          Authorization: `Bearer ${accessToken}`,
         },
         body: JSON.stringify({ query }),
       })
