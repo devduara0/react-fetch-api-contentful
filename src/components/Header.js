4c9439d10640c9519e44dd036319c73abda88e5f
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-21 14:46:16 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ import { Flex } from './styles/Flex.styled'
 import { Buttonbg } from './styles/Buttonbg'
 const dotenv =  require('dotenv');
 dotenv.config()
-const space = process.env.REAT_PUBLIC_CONTENTFUL_SPACE_IDtwo
-//const accessToken = process.env.REAT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+const space = process.env.REAT_PUBLIC_CONTENTFUL_SPACE_ID
+const accessToken = process.env.PUBLIC_CONTENTFUL_ACCESS
 const query = `
 {
    headerCollection{
