54622848733dc70c6011503c755f0e6b09de117f
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-25 17:20:30 +0300

    Update Header.styled.js

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -18,7 +18,7 @@ export const Nav = styled.nav`
 
 export const Logo = styled.img`
   @media (max-width: ${({ theme }) => theme.mobile}) {
-    margin-bottom: 40px;
+    margin-bottom: 10px;
   }
 `
 
