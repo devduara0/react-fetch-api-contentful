70d64a3f895959d7873733cc22604f10cacc466e
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-10-25 17:47:13 +0300

    Update Header.styled.js

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -9,7 +9,7 @@ export const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 10px;
+  margin-bottom: 40px;
 
   @media (max-width: ${({ theme }) => theme.mobile}) {
     flex-direction: column;
