1358bd43bd4c6f3c2c0867fba77b3508c40be51b
devduara0 <72891325+devduara0@users.noreply.github.com>
2021-11-02 15:50:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
-import Content from './components/Content
+import Content from './components/Content'
 import GlobalStyles from './components/styles/Global'
 import { Container } from './components/styles/Container.styled'
 
